504b883061c3f5f8623b4a20097632a9f3ce94b0
uV3301 <uv.gasni0303@gmail.com>
2021-04-13 20:09:03 +0530

    boilerplate

---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  background: #000;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+`;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Nav } from "./NavbarElements";
+const Navbar = () => {
+  return (
+    <>
+      <Nav>hey there</Nav>
+    </>
+  );
+};
+
+export default Navbar;
